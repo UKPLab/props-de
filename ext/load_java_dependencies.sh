7592a9c99ab5d2a89540ee8915b89da7b06297a9
Tobias Falke <falke@aiphes.tu-darmstadt.de>
2017-03-22 07:18:32 +0100

    JoBimText url updated

--- ext/load_java_dependencies.sh
+++ ext/load_java_dependencies.sh
@@ -12,7 +12,7 @@ wget 'https://drive.google.com/uc?export=download&id=0B-qbj-8rtoUMaUVsWUFuOE81ZW
 #wget 'https://docs.google.com/uc?export=download&id=0B-qbj-8rtoUMLUg5NGpBVW9JNkE' -O mate-model/parser-ger.model
 
 # JoBimText Dependency Collapsing
-wget 'http://maggie.lt.informatik.tu-darmstadt.de/jobimtext/wordpress/wp-content/uploads/2015/10/collapsing-asl.zip' -O collapsing-asl.zip
+wget 'http://ltmaggie.informatik.uni-hamburg.de/jobimtext/wordpress/wp-content/uploads/2015/10/collapsing-asl.zip' -O collapsing-asl.zip
 unzip collapsing-asl.zip
 mv collapsing-asl/org.jobimtext.collapsing.jar org.jobimtext.collapsing.jar 
 mv collapsing-asl/org.jobimtext.collapsing_lib org.jobimtext.collapsing_lib
